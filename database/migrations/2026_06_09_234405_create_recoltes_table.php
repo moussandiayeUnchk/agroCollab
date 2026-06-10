c22c0f910129f74264bb98e6c0809a4a6d5c0ce5
Moussa Ndiaye <moussan2003@gmail.com>
2026-06-10 00:09:38 +0000

    ajout des differents tables(recoltes, reservation,materiel,intrant) à travers la migration

---
+++ database/migrations/2026_06_09_234405_create_recoltes_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('recoltes', function (Blueprint $table) {
+            $table->id();
+            $table->string("categorieProduction");
+            $table->decimal('quantity', 8, 3);
+            $table->foreignIdFor(User::class)->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('recoltes');
+    }
+};
